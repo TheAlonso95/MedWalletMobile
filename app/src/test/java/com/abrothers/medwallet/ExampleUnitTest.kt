92b11946a144888c049d4d98ae191988bc9f5d93
Alonso <rafael.a.m95@hotmail.com>
2022-06-05 01:10:22 +0000

    feat: Replace project for jetpack compose

--- app/src/test/java/com/abrothers/medwallet/ExampleUnitTest.kt
+++ app/src/test/java/com/abrothers/medwallet/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.example.medwallet
+package com.abrothers.medwallet
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
