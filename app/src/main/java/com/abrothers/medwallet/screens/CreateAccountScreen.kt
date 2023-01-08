eaaf151e5c10fe74079461e0e57fe2b2c486d06c
Alonso <rafael.a.m95@hotmail.com>
2023-01-08 00:35:47 -0100

    feat: add ui card to hold the form

--- app/src/main/java/com/abrothers/medwallet/screens/CreateAccountScreen.kt
+++ app/src/main/java/com/abrothers/medwallet/screens/CreateAccountScreen.kt
@@ -1,16 +1,18 @@
 package com.abrothers.medwallet.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -18,16 +20,26 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun CreateAccountScreen(navController: NavController) {
 
-   Column(
-       Modifier
-           .fillMaxWidth()
-           .padding(12.dp)) {
-       Text(
-           text = "Create Account",
-           fontSize = MaterialTheme.typography.titleMedium.fontSize,
-           fontWeight = FontWeight.Bold
-       )
-   }
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primary) {
+        Column(
+            Modifier
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+            Card(modifier = Modifier
+                .weight(2f)
+                .padding(8.dp), shape = RoundedCornerShape(32.dp)) {
+                Column( Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center) {
+                    Text(text = "Create account", fontWeight = FontWeight.Bold, fontSize = 32.sp)
+
+                }
+            }
+        }
+    }
 }
 
 @Composable
