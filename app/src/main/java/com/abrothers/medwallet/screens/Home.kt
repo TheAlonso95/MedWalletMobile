da7a263ed1ff8aae14cd5d57ce268c1441c8b726
Alonso <rafael.a.m95@hotmail.com>
2023-01-13 23:11:14 -0100

    feat: implement lazyLoad row for accounts (WIP)

--- app/src/main/java/com/abrothers/medwallet/screens/Home.kt
+++ app/src/main/java/com/abrothers/medwallet/screens/Home.kt
@@ -1,10 +1,17 @@
 package com.abrothers.medwallet.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.abrothers.medwallet.components.AccountComponent
@@ -16,10 +23,23 @@ import com.abrothers.medwallet.ui.theme.MedWalletTheme
 @Composable
 fun HomeScreen(navController: NavController) {
     //NOTE: This is just for testing
-    val account = Account("1", "Rafael", "https://img.freepik.com/free-icon/user_318-790139.jpg?w=2000")
-    Column() {
-        Text(text = "Welcome xxx")
-        AccountComponent(account)
+    val account =
+        Account("1", "Rafael", "https://img.freepik.com/free-icon/user_318-790139.jpg?w=2000")
+    Box {
+        val accountsListState = rememberLazyListState()
+
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(text = "Welcome xxx")
+
+            LazyRow(
+                state = accountsListState,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(10) {
+                    AccountComponent(account)
+                }
+            }
+        }
     }
 }
 
