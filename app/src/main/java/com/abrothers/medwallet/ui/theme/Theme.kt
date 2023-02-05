ff1aed05c8287ad46f09324752c605bcc8bf5ea9
Alonso <rafael.a.m95@hotmail.com>
2023-02-05 22:34:59 -0100

    feat: replace deprecated getInsetsController

--- app/src/main/java/com/abrothers/medwallet/ui/theme/Theme.kt
+++ app/src/main/java/com/abrothers/medwallet/ui/theme/Theme.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -50,9 +50,15 @@ fun MedWalletTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
+
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
+
         SideEffect {
+            currentWindow.statusBarColor = colorScheme.primary.toArgb()
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
+                darkTheme
         }
     }
 
